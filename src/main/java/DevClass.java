d26f55e660ba09714759f9740d221afd75016133
kalpeshkm <114247760+kalpeshkm@users.noreply.github.com>
2024-02-12 15:16:08 +0530

    Updated
    Add

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Devclass + learn - Kalpesh ");
+        System.out.println("Devclass + learn - World");
         //checkin from learn
     }
 }
