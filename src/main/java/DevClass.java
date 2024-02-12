2e15e6cb5504bd790278805982a267d058cc455e
bhaskarbasu1 <159751414+bhaskarbasu1@users.noreply.github.com>
2024-02-12 12:53:51 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,5 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Devclass");
+        System.out.println("Devclass + learn ");
+        //checkin from learn
     }
 }
